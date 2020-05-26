6e62aed18cbfb7e09b89ef66f05b471ff931977c
yarbasha <yarbasha@gmail.com>
2020-05-26 11:33:55 +0300

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { FlatList, Text, ScrollView } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
-import { LEADERS } from '../shared/leaders';
-import { connect} from 'react-redux';
-import {baseUrl} from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 
 const mapStateToProps = state => {
-  return{
+  return {
     leaders: state.leaders
   }
 }
@@ -43,24 +43,45 @@ class About extends Component {
         title={item.name}
         subtitle={item.description}
         hideChevron={true}
-        leftAvatar={{ source: {uri: baseUrl+item.image} }}
+        leftAvatar={{ source: { uri: baseUrl + item.image } }}
       />
     );
   };
   render() {
-    return (
-      <ScrollView>
-        <History />
-        <Card title='Corporate Leadership'>
-          <FlatList
-            data={this.props.leaders.leaders}
-            renderItem={this.renderLeader}
-            keyExtractor={item => item.id.toString()}
-          />
-        </Card>
-      </ScrollView>
-
-    );
+    if (this.props.leaders.isLoading) {
+      return (
+        <ScrollView>
+          <History />
+          <Card title='Corporate Leadership'>
+            <Loading />
+          </Card>
+        </ScrollView>
+      );
+    }
+    else if (this.props.leaders.errMess) {
+      return (
+        <ScrollView>
+          <History />
+          <Card title='Corporate Leadership'>
+            <Text>{this.props.leaders.errMess}</Text>
+          </Card>
+        </ScrollView>
+      );
+    }
+    else {
+      return (
+        <ScrollView>
+          <History />
+          <Card title='Corporate Leadership'>
+            <FlatList
+              data={this.props.leaders.leaders}
+              renderItem={renderLeader}
+              keyExtractor={item => item.id.toString()}
+            />
+          </Card>
+        </ScrollView>
+      );
+    }
   }
 }
 
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { Tile } from 'react-native-elements';
-import {connect} from 'react-redux';
-import {baseUrl} from '../shared/baseUrl';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
-  return{
+	return {
 		dishes: state.dishes
-  }
+	}
 };
 
 class Menu extends Component {
@@ -16,28 +17,42 @@ class Menu extends Component {
 		title: 'Menu'
 	};
 
-	
+
 	render() {
 		const renderMenuItem = ({ item, index }) => {
-		return (
-			<Tile
-				key={index}
-				title={item.name}
-				caption={item.description}
-				featured
-				onPress={() => navigate('Dishdetail', { dishId: item.id })}
-				imageSrc={{ uri: baseUrl+ item.image }}
-			/>
-		);
-	};
+			return (
+				<Tile
+					key={index}
+					title={item.name}
+					caption={item.description}
+					featured
+					onPress={() => navigate('Dishdetail', { dishId: item.id })}
+					imageSrc={{ uri: baseUrl + item.image }}
+				/>
+			);
+		};
 		const { navigate } = this.props.navigation;
-		return (
-			<FlatList
-				data={this.props.dishes.dishes}
-				renderItem={renderMenuItem}
-				keyExtractor={item => item.id.toString()}
-			/>
-		);
+		if (this.props.dishes.isLoading) {
+			return (
+				<Loading />
+			);
+		}
+		else if (this.props.dishes.errMess) {
+			return (
+				<View>
+					<Text>{props.dishes.errMess}</Text>
+				</View>
+			);
+		}
+		else {
+			return (
+				<FlatList
+					data={this.props.dishes.dishes}
+					renderItem={renderMenuItem}
+					keyExtractor={item => item.id.toString()}
+				/>
+			);
+		}
 	}
 
 }
