b28d1043ea272e33d7bba334d91b288a941ba08e
yarbasha <yarbasha@gmail.com>
2020-05-31 21:58:08 +0300

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
-function Contact() {
-	return (
-		<ScrollView>
-			<Animatable.View useNativeDriver={true} animation="fadeInDown" duration={2000} delay={1000}>
-				<Card title='Contact Information'>
-					<Text style={{ margin: 5 }}>121, Clear Water Bay Road</Text>
-					<Text style={{ margin: 5 }}>Clear Water Bay, Kowloon</Text>
-					<Text style={{ margin: 5 }}>HONG KONG</Text>
-					<Text style={{ margin: 5 }}>Tel: +852 1234 5678</Text>
-					<Text style={{ margin: 5 }}>Fax: +852 8765 4321</Text>
-					<Text style={{ margin: 5 }}>Email:confusion@food.net</Text>
-				</Card>
-			</Animatable.View>
-		</ScrollView>
-	);
+class Contact extends Component {
+
+	sendMail() {
+		MailComposer.composeAsync({
+			recipients: ['confusion@food.net'],
+			subject: 'Enquiry',
+			body: 'To whom it may concern'
+		});
+	}
+
+	render() {
+		return (
+			<ScrollView>
+				<Animatable.View useNativeDriver={true} animation="fadeInDown" duration={2000} delay={1000}>
+					<Card title='Contact Information'>
+						<Text style={{ margin: 5 }}>121, Clear Water Bay Road</Text>
+						<Text style={{ margin: 5 }}>Clear Water Bay, Kowloon</Text>
+						<Text style={{ margin: 5 }}>HONG KONG</Text>
+						<Text style={{ margin: 5 }}>Tel: +852 1234 5678</Text>
+						<Text style={{ margin: 5 }}>Fax: +852 8765 4321</Text>
+						<Text style={{ margin: 5 }}>Email:confusion@food.net</Text>
+						<Button title=" Send Email"
+							buttonStyle={{ backgroundColor: '#512DA8' }}
+							icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+							onPress={this.sendMail} />
+					</Card>
+				</Animatable.View>
+			</ScrollView>
+		);
+	}
 }
 
 export default Contact;
