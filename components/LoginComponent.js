145a1ac079b5672ec0265cb5e99c6578d396b853
yarbasha <yarbasha@gmail.com>
2020-06-01 01:04:42 +0300

    Image Manipulator

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,9 @@ import * as Permissions from 'expo-permissions';
 import * as ImagePicker from 'expo-image-picker';
 import { createBottomTabNavigator } from 'react-navigation';
 import { baseUrl } from '../shared/baseUrl';
+import * as ImageManipulator from "expo-image-manipulator";
+import { Asset } from "expo-asset";
+
 
 class LoginTab extends Component {
 
@@ -116,10 +119,22 @@ class RegisterTab extends Component {
         aspect: [4, 3]
       });
       if (!capturedImage.cancelled) {
-        this.setState({ imageUrl: capturedImage.uri });
+        this.processImage(capturedImage.uri);
       }
     }
   }
+
+  processImage = async (imageUri) => {
+    let processedImage = await ImageManipulator.manipulateAsync(
+      imageUri,
+      [
+        { resize: { width: 400 } }
+      ],
+      { format: 'png' }
+    );
+    this.setState({ imageUrl: processedImage.uri });
+  }
+
   static navigationOptions = {
     title: 'Register',
     tabBarIcon: ({ tintColor }) => (
